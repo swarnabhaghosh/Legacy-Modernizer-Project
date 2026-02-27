cadb6b4bf0080443f9e689c98559ce0ceedf6800
Swarnabha <swarnabha983@gmail.com>
2026-02-27 21:32:08 +0530

    commit

--- graph/dependency_graph.py
+++ graph/dependency_graph.py
@@ -3,69 +3,38 @@ import json
 import javalang
 import networkx as nx
 
-
 def build_graph(repo_path):
-
     graph = nx.DiGraph()
-
     methods = {}
-
     for root, dirs, files in os.walk(repo_path):
-
         for file in files:
-
             if file.endswith(".java"):
-
                 path = os.path.join(root, file)
-
                 with open(path, "r") as f:
-
                     code = f.read()
-
                 tree = javalang.parse.parse(code)
-
                 for _, node in tree:
-
                     if isinstance(node, javalang.tree.MethodDeclaration):
-
                         methods[node.name] = node
-
                         graph.add_node(node.name)
 
     for name, node in methods.items():
-
         for _, child in node:
-
             if isinstance(child, javalang.tree.MethodInvocation):
-
                 called = child.member
-
                 if called in methods:
-
                     graph.add_edge(name, called)
-
     return graph
 
-
 def save_json(graph):
-
     data = {}
-
     for node in graph.nodes:
-
         data[node] = list(graph.neighbors(node))
-
     with open("graph.json", "w") as f:
-
         json.dump(data, f, indent=4)
 
-
 if __name__ == "__main__":
-
     repo = "repo"
-
     graph = build_graph(repo)
-
     save_json(graph)
-
     print("Graph saved to graph.json")
--- parser/code_parser.py
+++ parser/code_parser.py
@@ -2,36 +2,20 @@ import os
 import javalang
 
 def parse_repo(repo_path):
-
     all_methods = {}
-
     for root, dirs, files in os.walk(repo_path):
-
         for file in files:
-
             if file.endswith(".java"):
-
                 path = os.path.join(root, file)
-
                 with open(path, "r") as f:
-
                     code = f.read()
-
                 tree = javalang.parse.parse(code)
-
                 for _, node in tree:
-
                     if isinstance(node, javalang.tree.MethodDeclaration):
-
                         all_methods[node.name] = path
-
     return all_methods
 
-
 if __name__ == "__main__":
-
     repo = "repo"
-
     methods = parse_repo(repo)
-
     print(methods)
